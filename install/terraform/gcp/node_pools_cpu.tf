21ef4c6b8760a68077859e7e60dba10e11c055e2
Sam Stoelinga <sammiestoel@gmail.com>
2023-06-26 19:10:27 -0700

    add nodepool for system services

--- install/terraform/gcp/node_pools_cpu.tf
+++ install/terraform/gcp/node_pools_cpu.tf
@@ -1,3 +1,27 @@
+resource "google_container_node_pool" "system" {
+  # this nodepool runs services like kube-dns
+  name = "system"
+
+  cluster            = google_container_cluster.main.id
+  initial_node_count = 2
+
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 5
+  }
+
+  node_config {
+    machine_type = "e2-medium"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      initial_node_count
+    ]
+  }
+}
+
+
 resource "google_container_node_pool" "builder_1" {
   name = "builder-1"
 
