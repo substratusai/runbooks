80a49d8c81d627ef5755869b9cc8ce739565f385
Sam Stoelinga <sammiestoel@gmail.com>
2023-06-27 11:28:32 -0700

    scale GKE builder nodepool to 0 (#41)

--- install/terraform/gcp/node_pools_cpu.tf
+++ install/terraform/gcp/node_pools_cpu.tf
@@ -2,15 +2,15 @@ resource "google_container_node_pool" "builder_1" {
   name = "builder-1"
 
   cluster            = google_container_cluster.main.id
-  initial_node_count = 1
-  node_locations     = [var.zone]
+  initial_node_count = 0
 
   autoscaling {
-    min_node_count = 1
+    min_node_count = 0
     max_node_count = 5
   }
 
   node_config {
+    spot         = true
     machine_type = "n2d-standard-8"
     ephemeral_storage_local_ssd_config {
       local_ssd_count = 2
