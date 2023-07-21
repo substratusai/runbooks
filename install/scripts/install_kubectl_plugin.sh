deed87037d609ea505b9db406f9443b86d562ed5
Brandon Bjelland <brandon@atscale.run>
2023-07-21 00:04:49 -0700

    added plugin to install script

--- install/scripts/install_kubectl_plugin.sh
+++ install/scripts/install_kubectl_plugin.sh
@@ -9,8 +9,11 @@ LATEST_RELEASE=$(curl ${REPO}/releases -s |
 OS=$(uname -s)
 ARCH=$(uname -m | sed 's/aarch64/arm64/g')
 LATEST_OPEN_NOTEBOOK_ARTIFACT_URL=$(echo $LATEST_RELEASE | awk -v repo=$REPO -v os=$OS -v arch=$ARCH -v release=$LATEST_RELEASE '{print repo "/releases/download/" release "/kubectl-open-notebook_" os "_" arch ".tar.gz"}')
-LATEST_UPLOAD_KIND_ARTIFACT_URL=$(echo $LATEST_RELEASE | awk -v repo=$REPO -v os=$OS -v arch=$ARCH -v release=$LATEST_RELEASE '{print repo "/releases/download/" release "/kubectl-open-notebook_" os "_" arch ".tar.gz"}')
+LATEST_UPLOAD_KIND_ARTIFACT_URL=$(echo $LATEST_RELEASE | awk -v repo=$REPO -v os=$OS -v arch=$ARCH -v release=$LATEST_RELEASE '{print repo "/releases/download/" release "/kubectl-upload-resource_" os "_" arch ".tar.gz"}')
 
-wget -qO- ${LATEST_ARTIFACT_URL} | tar zxv
+wget -qO- ${LATEST_OPEN_NOTEBOOK_ARTIFACT_URL} | tar zxv
+wget -qO- ${LATEST_UPLOAD_KIND_ARTIFACT_URL} | tar zxv
 chmod +x kubectl-open-notebook
+chmod +x kubectl-upload-resource
 mv kubectl-open-notebook /usr/local/bin/ || sudo mv kubectl-open-notebook /usr/local/bin/
+mv kubectl-upload-resource /usr/local/bin/ || sudo mv kubectl-upload-resource /usr/local/bin/
