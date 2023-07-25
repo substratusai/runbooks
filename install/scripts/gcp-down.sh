f9ff0a978165c4942a58a045b66844b9c6396793
Brandon J. Bjelland <brandon@atscale.run>
2023-07-25 13:40:11 -0700

    add auto-approve for automated testing (#143)
    adds a headless/non-interactive mode for creating and destroying s8s infrastructure.

--- install/scripts/gcp-down.sh
+++ install/scripts/gcp-down.sh
@@ -10,18 +10,23 @@ set -u
 export CLOUDSDK_AUTH_ACCESS_TOKEN=${TOKEN}
 # Used by terraform:
 export GOOGLE_OAUTH_ACCESS_TOKEN=${TOKEN}
+AUTO_APPROVE="${AUTO_APPROVE:-no}"
 
-tf_bucket=${PROJECT}-substratus-terraform
+TF_BUCKET=${PROJECT}-substratus-terraform
 
 # Delete infrastructure.
 cd terraform/gcp
 
 # Backend variables cannot be configured via env variables.
-echo "bucket = \"${tf_bucket}\"" >>backend.tfvars
+echo "bucket = \"${TF_BUCKET}\"" >>backend.tfvars
 terraform init --backend-config=backend.tfvars
 
 export TF_VAR_project_id=${PROJECT}
-terraform destroy
+if [ "${AUTO_APPROVE}" == "yes" ]; then
+  terraform destroy -auto-approve
+else
+  terraform destroy
+fi
 cluster=$(terraform output --raw cluster_name)
 
 cd -
--- install/scripts/gcp-up.sh
+++ install/scripts/gcp-up.sh
@@ -11,24 +11,29 @@ export CLOUDSDK_AUTH_ACCESS_TOKEN=${TOKEN}
 # Used by terraform:
 export GOOGLE_OAUTH_ACCESS_TOKEN=${TOKEN}
 INSTALL_OPERATOR="${INSTALL_OPERATOR:-yes}"
+AUTO_APPROVE="${AUTO_APPROVE:-no}"
 
 # Enable required services.
 gcloud services enable --project ${PROJECT} container.googleapis.com
 gcloud services enable --project ${PROJECT} artifactregistry.googleapis.com
 
 # Create terraform state bucket if one does not exist.
-tf_bucket=${PROJECT}-substratus-terraform
-gcloud storage buckets describe gs://${tf_bucket} >/dev/null || gcloud storage buckets create --project ${PROJECT} gs://${tf_bucket}
+TF_BUCKET=${PROJECT}-substratus-terraform
+gcloud storage buckets describe gs://${TF_BUCKET} >/dev/null || gcloud storage buckets create --project ${PROJECT} gs://${TF_BUCKET}
 
 # Apply infrastructure.
 cd terraform/gcp
 
 # Backend variables cannot be configured via env variables.
-echo "bucket = \"${tf_bucket}\"" >>backend.tfvars
+echo "bucket = \"${TF_BUCKET}\"" >>backend.tfvars
 terraform init --backend-config=backend.tfvars
 
 export TF_VAR_project_id=${PROJECT}
-terraform apply
+if [ "${AUTO_APPROVE}" == "yes" ]; then
+  terraform apply -auto-approve
+else
+  terraform apply
+fi
 cluster_name=$(terraform output --raw cluster_name)
 cluster_region=$(terraform output --raw cluster_region)
 
@@ -40,7 +45,7 @@ gcloud container clusters get-credentials --project ${PROJECT} --region ${cluste
 kubectl apply -f https://raw.githubusercontent.com/GoogleCloudPlatform/container-engine-accelerators/master/nvidia-driver-installer/cos/daemonset-preloaded-latest.yaml
 
 # Install cluster components.
-if [ "$INSTALL_OPERATOR" == "yes" ]; then
+if [ "${INSTALL_OPERATOR}" == "yes" ]; then
   kubectl apply -f kubernetes/namespace.yaml
   kubectl apply -f kubernetes/config.yaml
   kubectl apply -f kubernetes/system.yaml
