713d0a049c3e75057ca4078bd8b456556ec977f4
Nick Stogner <nicholas.stogner@gmail.com>
2023-07-29 14:44:27 -0400

    Cleanup old cmds

--- internal/controller/utils.go
+++ internal/controller/utils.go
@@ -3,9 +3,6 @@ package controller
 import (
 	"context"
 	"fmt"
-	"math"
-	"net/url"
-	"path/filepath"
 	"strings"
 
 	batchv1 "k8s.io/api/batch/v1"
@@ -28,30 +25,6 @@ func nextPowOf2(n int64) int64 {
 		k = k << 1
 	}
 	return k
-}
-
-const (
-	thousand = 1000
-	million  = 1000 * 1000
-	billion  = 1000 * 1000 * 1000
-
-	gigabyte = int64(1024 * 1024 * 1024)
-)
-
-func roundUpGB(bytes int64) int64 {
-	return int64(math.Ceil(float64(bytes)/float64(gigabyte))) * gigabyte
-}
-
-func parseBucketURL(bucketURL string) (string, string, error) {
-	u, err := url.Parse(bucketURL)
-	if err != nil {
-		return "", "", fmt.Errorf("parsing bucket url: %w", err)
-	}
-
-	bucket := u.Host
-	subpath := strings.TrimPrefix(filepath.Dir(u.Path), "/")
-
-	return bucket, subpath, nil
 }
 
 func reconcileJob(ctx context.Context, c client.Client, job *batchv1.Job, condition string) (result, error) {
