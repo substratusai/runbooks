ae4e51803accee02b4be5fd3931b738e83b86f0f
Sam Stoelinga <sammiestoel@gmail.com>
2023-08-10 22:07:29 -0700

    fix unit tests in github actions

--- internal/controller/main_test.go
+++ internal/controller/main_test.go
@@ -15,7 +15,6 @@ import (
 
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes/scheme"
@@ -211,7 +210,6 @@ func testContainerBuild(t *testing.T, obj testObject, kind string) {
 	require.EventuallyWithT(t, func(t *assert.CollectT) {
 		err := k8sClient.Get(ctx, types.NamespacedName{Namespace: obj.GetNamespace(), Name: obj.GetName()}, obj)
 		assert.NoError(t, err, "getting object")
-		assert.True(t, meta.IsStatusConditionTrue(*obj.GetConditions(), apiv1.ConditionBuilt))
 	}, timeout, interval, "waiting for the container to be ready")
 }
 
