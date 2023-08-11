3555d4b993c3ee7f51db14e6a3f8a039e243ee34
Sam Stoelinga <sammiestoel@gmail.com>
2023-08-10 22:34:29 -0700

    remove all TF resources except cluster + NPs

--- install/terraform/gcp/buckets.tf
+++
@@ -1,8 +0,0 @@
-resource "google_storage_bucket" "artifacts" {
-  project       = var.project_id
-  name          = "${var.project_id}-${local.name}-artifacts"
-  location      = var.region
-  force_destroy = true
-
-  uniform_bucket_level_access = true
-}
--- install/terraform/gcp/service_account_iam.tf
+++
@@ -1,7 +0,0 @@
-# allow the service account to create signed URLs via the iam.serviceAccounts.signBlob permission
-
-resource "google_service_account_iam_member" "gcp_manager_self_token_creator" {
-  service_account_id = google_service_account.gcp_manager.name
-  role               = "roles/iam.serviceAccountTokenCreator"
-  member             = "serviceAccount:${google_service_account.gcp_manager.email}"
-}
