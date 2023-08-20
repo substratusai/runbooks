38f1655b869965d9d72231cad09e88493f341bef
Sam Stoelinga <sammiestoel@gmail.com>
2023-08-20 13:27:39 -0700

    fix error message

--- install/scripts/kind-gpu-up.sh
+++ install/scripts/kind-gpu-up.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
+# This is a workaround for GPU support in kind:
+# https://github.com/kubernetes-sigs/kind/pull/3257#issuecomment-1607287275
 
 set -e
 set -u
 
-# This is a workaround for GPU support in kind:
-# https://github.com/kubernetes-sigs/kind/pull/3257#issuecomment-1607287275
 if ! docker info | grep -q "Default Runtime: nvidia"; then
-  echo "nvidia needs to be the default runtime to configure kind with GPU support."
+  echo "ERROR: nvidia needs to be the default runtime to configure kind with GPU support."
   echo "See steps here for more details: https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/user-guide.html#adding-the-nvidia-runtime"
   echo "In most cases running the follow steps will configure nvidia as the default runtime:"
-  echo "sudo nvidia-ctk runtime configure --runtime=docker"
+  echo "sudo nvidia-ctk runtime configure --runtime=docker --set-as-default"
   echo "sudo systemctl restart docker"
+  exit 1
 fi
 
 nvidia_config_file="/etc/nvidia-container-runtime/config.toml"
 if [ -e "${nvidia_config_file}" ]; then
   sudo sed -i '/accept-nvidia-visible-devices-as-volume-mounts/c\accept-nvidia-visible-devices-as-volume-mounts = true' ${nvidia_config_file}
 else
-  echo "error: Missing file ${nvidia_config_file}, which likely means you have not configured the nvidia container runtime."
+  echo "ERROR: Missing file ${nvidia_config_file}, which likely means you have not configured the nvidia container runtime."
   echo "You can follow the install steps here to install the NVIDIA container toolkit: https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html"
   exit 1
 fi
-
 
 kind create cluster --name substratus --config - <<EOF
 apiVersion: kind.x-k8s.io/v1alpha4
